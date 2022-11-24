2e341a54298a885649af1e9ce9c41af32644becd
timohaverich <timo.haverich@fh-bielefeld.de>
2022-11-24 11:02:00 +0100

    Finished

--- src/Bruch.java
+++ src/Bruch.java
@@ -1,37 +1,59 @@
 public class Bruch {
 
+    // Instanzvariablen für Zähler und Nenner
     private int zaehler;
     private int nenner;
 
+    // Konstruktor für Bruch
     public Bruch (int zaehler, int nenner) {
+
+        // prüft, ob der Nenner gleich 0 ist
+        if (nenner == 0) {
+            System.out.println("Es darf nicht durch 0 geteilt werden.");
+            System.exit(0);
+        }
+
+        // Werte setzen
         this.zaehler = zaehler;
         this.nenner = nenner;
     }
 
+    // gibt den Zähler zurück
     public int getZaehler() {
         return this.zaehler;
     }
+
+    // gibt den Nenner zurück
     public int getNenner() {
         return this.nenner;
     }
 
+    // Addition von zwei Brüchen
     public Bruch add (Bruch f) {
+        // berechnet den Zähler
         int newZaehler = (zaehler * f.nenner) + (f.zaehler * nenner);
+        // berechnet den Nenner
         int newNenner = (f.nenner * nenner);
 
+        // erzeugt neues Bruch-Objekt und gibt dieses zurück
         return new Bruch(newZaehler, newNenner);
     }
 
+    // Multiplikation von zwei Brüchen
     public Bruch multiply (Bruch f) {
+        // berechnet den Zähler
         int newZaehler = (zaehler * f.zaehler);
+
+        // berechnet den Nenner
         int newNenner = (nenner * f.nenner);
 
+        // erzeugt neues Bruch-Objekt und gibt dieses zurück
         return new Bruch(newZaehler, newNenner);
     }
 
+    // gibt Zähler und Nenner als String zurück
     public String toString () {
         return zaehler + "/" + nenner;
     }
-
 
 }
--- src/BruchTest.java
+++ src/BruchTest.java
@@ -1,13 +1,22 @@
 public class BruchTest {
     public static void main(String[] args) {
 
+        // Testbrüche
         Bruch a = new Bruch(5,3);
         Bruch f = new Bruch(2, 2);
 
-        Bruch ergebnisBruchAddition = a.add(f);
-        System.out.println(ergebnisBruchAddition.toString());
+        // Ausgabe der Brüche zur Berechnung
+        System.out.println("Bruch 1: " + a);
+        System.out.println("Bruch 2: " + f + '\n');
 
+        // Bruchberechnung: Addition
+        Bruch ergebnisBruchAddition = a.add(f);
+        System.out.println("Ergebnis Addition: ");
+        System.out.println(ergebnisBruchAddition.toString()  + '\n');
+
+        // Bruchberechnung: Multiplikation
         Bruch ergebnisBruchMultiplikation = a.multiply(f);
+        System.out.println("Ergebnis Multiplikation: ");
         System.out.println(ergebnisBruchMultiplikation.toString());
 
     }
